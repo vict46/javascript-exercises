aeb061d1766c414bf462380ac525c9eddec508d2
Victor <sirinovic@gmail.com>
2022-08-21 22:53:13 +1000

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let output;
+    if (num < 0) {
+        output = "ERROR";
+    } else {
+        output = "";
+    };
+    
+    for (let index = 0; index < num; index++) {
+        output = output + string;            
+        };
 
+    return output;
 };
 
 // Do not edit below this line
