412aa8b8813a5f732b63c23b73242eec38b15b72
Victor <sirinovic@gmail.com>
2022-08-23 22:49:29 +1000

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reverseInput = ""
+    for (let index = 0; index < input.length; index++) {
+        let letter = input.charAt(index);
+        if (index == 0) {
+            reverseInput = letter;
+        } else {
+            reverseInput = letter + reverseInput;
+        }  
+    }
+    return reverseInput;
 };
 
 // Do not edit below this line
