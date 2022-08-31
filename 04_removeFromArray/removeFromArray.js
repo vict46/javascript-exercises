d2313e3e1cd20c0729bc466b229cd37a34daa42e
Victor <sirinovic@gmail.com>
2022-08-31 22:11:35 +1000

    updated exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(fullArray) {
+    let args = [];
+    for (let index = 1; index < arguments.length; index++) {
+        args[index-1] = arguments[index];
+    }
+    filteredArray = fullArray.filter(matchArrayValue);
 
+    function matchArrayValue(checkValue) {
+        args.includes(checkValue);
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line
