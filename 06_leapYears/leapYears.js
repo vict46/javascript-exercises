a78b6a3744da84ca359387a5efc0a863f70d580c
Vic <sirinovic@gmail.com>
2022-09-02 21:54:28 +1000

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapYear;
+    if (
+        year % 400 == 0) {
+    leapYear = true;
+} else if (
+    year % 4 == 0 &&
+    year % 100 !== 0) {
+    leapYear = true;
+} else {
+    leapYear = false;
+}
+return leapYear;
 };
 
 // Do not edit below this line
