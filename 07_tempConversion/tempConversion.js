a108a8f736f1c2f5be9823f69cf6300c224537ff
Vic <sirinovic@gmail.com>
2022-09-02 22:02:36 +1000

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let ctemp = (ftemp - 32) * 5 / 9;
+  return (Math.round(ctemp * 10) / 10);
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  let ftemp = ctemp * 9 / 5 + 32;
+  return (Math.round(ftemp * 10) / 10); 
 };
 
 // Do not edit below this line
