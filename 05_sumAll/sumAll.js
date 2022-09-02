8239a2a88a93c4f3d4eb8cb54b88cdcd10c0a9ea
Vic <sirinovic@gmail.com>
2022-09-02 21:33:32 +1000

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let small;
+    let large;
+    let total;
 
+    switch (true) {
+        case typeof(first) !==  "number":
+        case typeof(last) !== "number":
+        case first < 0:
+        case last < 0:
+            total = "ERROR";            
+            break;
+        default:
+            total = 0;
+            
+            if (last < first) {
+                small = last;
+                large = first;
+            } else {
+                small = first;
+                large = last;
+            }        
+            
+            for (let index = small; index < large + 1; index++) {
+                total = total + index;
+            }
+            break;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
