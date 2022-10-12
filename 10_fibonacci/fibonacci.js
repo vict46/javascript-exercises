4a15fd3f2908ad969c0e1cb7110853c30b6ecff1
Vic <sirinovic@gmail.com>
2022-10-12 21:51:48 +1100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n == 1) { return 1}
+    if (n < 1) {return "OOPS"}
+    let increment = 0;
+    let sum = 1 + increment;
+    for (let index = 0; index < n-1; index++) {
+        prevsum = sum
+        sum = sum + increment;
+        increment = prevsum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// n = 1, sum = 1, inc = 1
+// n = 2, sum = 2, inc = 2
