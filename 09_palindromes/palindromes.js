6a66e3c63cb6f12f853e5863eb3e04c2b57d1ac5
Vic <sirinovic@gmail.com>
2022-10-12 21:54:16 +1100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    phrase = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()/\s]/g,"").toLowerCase();
+    return reverseString(phrase) == phrase;
 };
+
+
+const reverseString = function(input) {
+    let reverseInput = ""
+    for (let index = 0; index < input.length; index++) {
+        let letter = input.charAt(index);
+        if (index == 0) {
+            reverseInput = letter;
+        } else {
+            reverseInput = letter + reverseInput;
+        }  
+    }
+    return reverseInput;
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
