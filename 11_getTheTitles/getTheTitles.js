b3e4c1e7bdd022f294530ecd6758a34c5b51d0f8
Vic <sirinovic@gmail.com>
2022-10-13 21:10:27 +1100

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map(books => books.title)
+    return titles
 };
 
 // Do not edit below this line
