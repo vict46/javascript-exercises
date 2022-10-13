6d313bfea67a327bcc208c297cf5885f595da3a9
Vic <sirinovic@gmail.com>
2022-10-13 22:07:40 +1100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b){
+        const currentYear = new Date().getFullYear();
+        const lastPerson = (a.hasOwnProperty('yearOfDeath') ? a.yearOfDeath : currentYear) - a.yearOfBirth;
+        const nextPerson = (b.hasOwnProperty('yearOfDeath') ? b.yearOfDeath : currentYear) - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
